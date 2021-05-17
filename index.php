4f69e8a86203d537e05753109a3ab988797cce11
Alejandro Zarate <71047000+ZamaxCode@users.noreply.github.com>
2021-05-17 16:45:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="icon" type="icon/png" href="http://pngimg.com/uploads/poop/poop_PNG10.png">
+
+	<title>My First Page</title>
+</head>
+<body>
+	<nav>
+		<h1>Welcome To This Shitty Page</h1>
+	</nav>
+
+	<h3>No se programar en web</h3>
+	<h4>(Me caga)</h4>
+	<div>
+		<p>
+		Pero bueno, por lo menos este es un ejemplo suficiente para saber si funciona el openshift ese del cual el profe en lugar de enseñarnos, prefiere que busquemos por nuestra cuenta en tutoriales de mierda que hay por youtube.<br><br>
+		La ventaja es que cualquer wey random en internet enseña mejor que el profe, pero no hay pedo ya que nos cae bien porque no es exigente, podria haber sido Campos.
+	</p>
+	</div>
+</body>
+</html>
